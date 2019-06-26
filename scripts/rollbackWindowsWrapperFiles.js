365806eaa2f9f8dc666a2bf9d12981b1e44d053d
Lindsay-Needs-Sleep <lindsay@muslimkids.tv>
2019-06-26 03:40:25 -0600

    Made compatible with cordova 9.0.0

--- scripts/rollbackWindowsWrapperFiles.js
+++ scripts/rollbackWindowsWrapperFiles.js
@@ -50,7 +50,7 @@ function configureParser(context) {
     ConfigParser = context.requireCordovaModule('cordova-lib/src/configparser/ConfigParser');
   }
 
-  etree = context.requireCordovaModule('cordova-lib/node_modules/elementtree');
+  etree = require('cordova-lib/node_modules/elementtree');
 
   var xml = cordova_util.projectConfig(context.opts.projectRoot);
   config = createConfigParser(xml, etree, ConfigParser);
@@ -70,7 +70,7 @@ module.exports = function (context) {
     return;
   }
   
-  Q = context.requireCordovaModule('q');
+  Q = require('q');
   var task = Q.defer();
 
   var destPath = path.join(projectRoot, "platforms", "windows", "www", "wrapper.html");
